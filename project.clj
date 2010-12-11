9ceef6a2d18d76c7c8e1fdef875520e531454da7
abscondment <brendan.ribera@gmail.com>
2010-12-11 13:41:13 -0800

    Separate clog from blog.

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject clog "0.5"
-  :repositories {"scala-tools" "http://scala-tools.org/repo-releases/"}
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [enlive "1.0.0-SNAPSHOT"]
-                 [clj-yaml "0.2.0-SNAPSHOT"]
-                 [org.markdownj/markdownj "0.3.0-1.0.2b4"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
-  :main clog.core)
