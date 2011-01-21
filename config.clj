cd6f5b28742600e0531f97098c22f9c24c1bd4b0
abscondment <brendan.ribera@gmail.com>
2011-01-21 10:09:35 -0800

    Changes for clog/config.

---
+++ config.clj
@@ -0,0 +1,8 @@
+{:title "Quod erat faciendum"
+ :subtitle "A technical blog by Brendan Ribera"
+ :author "Brendan Ribera"
+ :domain "threebrothers.org"
+ :path "/home/brendan/code/threebrothers.org"
+ :static-paths ["/brendan/"
+                "/brendan/about/"
+                "/brendan/software/"]}
