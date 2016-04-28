4608210aaffa7c1125c8d663d723409a58bbde1a
Brendan Ribera <brendan.ribera@gmail.com>
2016-04-28 10:06:30 -0700

    stub

---
+++ shell.nix
@@ -0,0 +1,27 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, hakyll, stdenv }:
+      mkDerivation {
+        pname = "threebrothers-org";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [ base hakyll ];
+        executableToolDepends = [ pkgs.darwin.apple_sdk.frameworks.Cocoa ];
+        license = stdenv.lib.licenses.unfree;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
